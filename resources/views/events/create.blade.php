72ac2db78d866d7dd79ea7002dfb5a94ea0a34a3
lucasff21 <lucasfreitas_c5@hotmail.com>
2022-06-14 00:03:52 -0300

    ajustando projeto

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events')
+
+@section('content')
+
+<h1>Crie um evento</h1>
+
+@endsection
+    
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+
+
+@foreach ($events as $event)
+    <p>{{$event->title}} -- {{$event->city}}</p>
+@endforeach
+
+
